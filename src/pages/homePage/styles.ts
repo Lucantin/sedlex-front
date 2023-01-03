bc4a8023969c1282495fec39b7e59dfe19ec4b2d
Lucantin <luccabento@gmail.com>
2023-01-03 00:28:04 -0300

    refactor: formating and remove unused

--- src/pages/homePage/styles.ts
+++ src/pages/homePage/styles.ts
@@ -1,8 +1,8 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
   flex-direction: row;
-`
+`;
 
 export const Start = styled.div`
   display: flex;
@@ -12,13 +12,13 @@ export const Start = styled.div`
   width: 100%;
   height: 31.25rem;
   background: linear-gradient(#1184e0, #ffffff);
-`
+`;
 
 export const Camera = styled.a`
   display: flex;
   text-decoration: none;
   font-size: 2.5rem;
-`
+`;
 
 export const GroupImage = styled.div`
   display: flex;
@@ -27,13 +27,13 @@ export const GroupImage = styled.div`
   width: 31.25rem;
   margin: 0rem auto;
   background-color: blue;
-`
+`;
 
 export const ImgStart = styled.img`
   display: flex;
   width: 100%;
   height: 100%;
-`
+`;
 
 export const Group = styled.div`
   display: flex;
@@ -41,36 +41,33 @@ export const Group = styled.div`
   flex-direction: column;
   padding: 1rem;
   width: 31.25rem;
-  font-size: 1rem;
   align-items: center;
   margin: 0rem auto;
-`
+`;
 
 export const TitleStart = styled.h1`
   display: flex;
   font-size: 1.5rem;
   padding: 1rem;
   text-align: center;
-  background-color: #000000;
-  opacity: 0.33;
+  background-color: rgba(0, 0, 0, 0.55);
   margin-top: 4.7rem;
   border-radius: 1rem;
-`
+`;
 
 export const TextStart = styled.p`
   display: flex;
   margin-top: 1rem;
   text-align: center;
-  font-size: 1rem;
   border-radius: 1rem;
-  background-color: #000000;
+  background-color: rgba(0, 0, 0, 0.55);
+  line-height: 1.3em;
   margin-left: 1rem;
   margin-right: 1rem;
-  padding: 1rem;
-  opacity: 0.33;
+  padding: 1.3rem;
   max-width: 100%;
   min-width: 10%;
-`
+`;
 export const Button = styled.a`
   width: 9rem;
   margin-top: 1em;
@@ -80,7 +77,7 @@ export const Button = styled.a`
   background-color: #40a9eb;
   align-items: center;
   padding: 0.3rem;
-`
+`;
 
 export const End = styled.div`
   display: flex;
@@ -89,7 +86,7 @@ export const End = styled.div`
   left: 0;
   width: 100%;
   height: 31.25rem;
-`
+`;
 
 export const Partner = styled.div`
   display: grid;
@@ -98,7 +95,7 @@ export const Partner = styled.div`
   left: 0;
   width: 100%;
   height: 31.25rem;
-`
+`;
 
 export const GroupPartner = styled.div`
   display: flex;
@@ -106,10 +103,9 @@ export const GroupPartner = styled.div`
   flex-direction: column;
   padding: 1rem;
   width: 100%;
-  font-size: 1rem;
   align-items: center;
   margin: 0rem auto;
-`
+`;
 
 export const TitlePartner = styled.h1`
   display: flex;
@@ -119,7 +115,7 @@ export const TitlePartner = styled.h1`
   color: #000000;
   margin-top: 4.7rem;
   border-radius: 1rem;
-`
+`;
 
 export const GroupCompanion = styled.div`
   display: flex;
@@ -128,9 +124,9 @@ export const GroupCompanion = styled.div`
   height: 12rem;
   width: 100%;
   margin: 0rem auto;
-`
+`;
 
 export const Companion = styled.img`
   display: flex;
   padding: 2rem auto;
-`
+`;
