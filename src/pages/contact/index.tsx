0e77e20a478a86db038a57527438eb7d5a9894e7
Lucantin <luccabento@gmail.com>
2023-01-18 13:13:51 -0300

    refactor: change style and formating

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -69,7 +69,7 @@ export const Contact = () => {
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.2447577704224!2d-46.76606658442418!3d-23.52369766608889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ceffa0e6c7b5a9%3A0xfb54dbcb83f84929!2sHavan%20Marginal%20Tiet%C3%AA%20(Osasco)!5e0!3m2!1spt-BR!2sbr!4v1673719682902!5m2!1spt-BR!2sbr"
           width="600"
           height="450"
-        ></S.Map>
+        />
       </S.WrapperMap>
     </S.Container>
   );
