e26fe78e2eda38304e71ab64d8ff552eda8f8919
André Souza <andre152803@outlook.com>
2023-02-02 19:34:57 -0300

    refactor: responsive map

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,39 +1,43 @@
-import { useState } from "react";
-import emailjs from "@emailjs/browser";
-import * as S from "./styles";
+import { useState } from 'react'
+import emailjs from '@emailjs/browser'
+import * as S from './styles'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
 
   function sendEmail(e: { preventDefault: () => void }) {
-    e.preventDefault();
+    e.preventDefault()
 
     const templateParams = {
       from_name: name,
       message: message,
       email: email,
-    };
+    }
 
     emailjs
       .send(
-        "service_d56jjho",
-        "template_tqptyuo",
+        'service_d56jjho',
+        'template_tqptyuo',
         templateParams,
-        "4wHZi7KaoGvAyI-HY"
+        '4wHZi7KaoGvAyI-HY',
       )
       .then(
         (res) => {
-          console.log("Email enviado", res.status, res.text);
-          setName("");
-          setEmail("");
-          setMessage("");
+          toast.success('Sucesso')
+          console.log('Email enviado', res.status, res.text)
+          setName('')
+          setEmail('')
+          setMessage('')
         },
         (err) => {
-          console.log("Erro: ", err);
-        }
-      );
+          toast.error('Algo deu errado!')
+          console.log('Erro: ', err)
+        },
+      )
   }
 
   return (
@@ -60,6 +64,7 @@ export const Contact = () => {
             value={message}
           />
           <S.SubmitButton>Enviar</S.SubmitButton>
+          <ToastContainer />
         </S.SendMail>
       </S.WrapperMail>
       <S.WrapperMap>
@@ -67,10 +72,10 @@ export const Contact = () => {
         <S.InfoMap>Rua casa da manutenção 2500</S.InfoMap>
         <S.Map
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.2447577704224!2d-46.76606658442418!3d-23.52369766608889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ceffa0e6c7b5a9%3A0xfb54dbcb83f84929!2sHavan%20Marginal%20Tiet%C3%AA%20(Osasco)!5e0!3m2!1spt-BR!2sbr!4v1673719682902!5m2!1spt-BR!2sbr"
-          width="600"
-          height="450"
+          width="100%"
+          height="100%"
         />
       </S.WrapperMap>
     </S.Container>
-  );
-};
+  )
+}
