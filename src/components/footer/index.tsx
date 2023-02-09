ce35ab68098b855c11ab9f7225d3a25471a21a9b
André Souza <andre152803@outlook.com>
2023-02-09 17:32:40 -0300

    refactor: number change

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { FaFacebookF } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa";
-import * as S from "./styles";
+import React from 'react'
+import { FaFacebookF } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaWhatsapp } from 'react-icons/fa'
+import * as S from './styles'
 
 const Footer = () => {
   return (
@@ -22,20 +22,29 @@ const Footer = () => {
       <S.General>
         <S.Title>Contatos</S.Title>
         <S.SubTitle>
-          <S.IconF href="http://www.facebook.com" target="_blank">
+          <S.IconF
+            href="https://ne-np.facebook.com/people/sedlex_manutencoes/100063797390672/"
+            target="_blank"
+          >
             <FaFacebookF />
           </S.IconF>
-          <S.IconI href="https://www.instagram.com/" target="_blank">
+          <S.IconI
+            href="https://www.instagram.com/sedlex_manutencoes/"
+            target="_blank"
+          >
             <FaInstagram />
           </S.IconI>
-          <S.IconW href="https://www.whatsapp.com/?lang=pt_br" target="_blank">
+          <S.IconW
+            href="https://l.instagram.com/?u=https%3A%2F%2Fwa.me%2Fmessage%2FTHHX7ITEOM6DI1&e=AT0ca2Bg93G1XzHW8IGFvBf2KmRPe85QGBmttPoqaLAFS3XZR9T2bCZ3a5aNxZmj5p5WWQ8sZWEjZEf8cXUxsDI8C5CTn6X1l43bwvlebWGmrB12&s=1"
+            target="_blank"
+          >
             <FaWhatsapp />
           </S.IconW>
         </S.SubTitle>
-        <S.Telephone>Telefone (11)987878788</S.Telephone>
+        <S.Telephone>Telefone (11) 95780-42127</S.Telephone>
       </S.General>
     </S.Container>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
